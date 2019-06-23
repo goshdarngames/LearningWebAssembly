db4a29ba7008813f8bfbd23bb9f8b0616b40e4da
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-23 12:00:14 +0100

    Complex number structs in place.

--- 003-mandelbrot/mandelbrot.c
+++ 003-mandelbrot/mandelbrot.c
@@ -1,15 +1,51 @@
 #include <emscripten.h>
 
+/****************************************************************************
+ * Complex Numbers
+ *---------------------------------------------------------------------------
+ * Complex numbers are numbers that consist of a pair of values rather than
+ * a single value.
+ ***************************************************************************/
+
+struct Complex
+{
+    float real;
+    float imag;
+};
+
+struct Complex mult ( struct Complex c1, struct Complex c2 )
+{
+    struct Complex r = { 0, 0 };
+
+    return r;
+}
+
+struct Complex add ( struct Complex c1, struct Complex c2 )
+{
+    struct Complex r = { 0, 0 };
+
+    return r;
+}
+
+/****************************************************************************
+ * Exported function
+ * - This function will be availabled in the Javascript code.
+ ***************************************************************************/
+
 EMSCRIPTEN_KEEPALIVE
 int mandelbrot ( int numIter, float targetX, float targetY, float range,
                  int gridSize, int x, int y )
 {
     int n = numIter;
 
-    float minX = targetX-(range/2);
-    float minY = targetY-(range/2);
+    struct Complex z = { 0, 0 };
 
-    float z[] = { 0, 0 };
+    //The value of C is calculated as an offset from the target coord
+    //by a fraction of the total 'range' of values within the grid
+    float cReal = targetX-(range/2)+(range/gridSize)*x;
+    float cImag = targetY-(range/2)+(range/gridSize)*y;
+
+    struct Complex c = { cReal, cImag };
 
     while ( n > 0 )
     {
