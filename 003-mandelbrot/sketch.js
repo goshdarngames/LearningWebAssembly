a169199014721d33244f62d37599be0a59c091e7
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-25 17:37:47 +0100

    Remove < br >s

--- 003-mandelbrot/sketch.js
+++ 003-mandelbrot/sketch.js
@@ -1,61 +1,68 @@
 ( function ( p5js_sketch, undefined )
 {
+    /**
+     * Stores data for the render process in a public field so that it
+     * may be displayed on the GUI.
+     */
+    p5js_sketch.render_data =
+    {
+        numIter  : 300,
+        targetX  : -0.7436,
+        targetY  : 0.1102,
+        range    : 0.005,
+        gridSize : [ 800, 800 ],
+        image    : undefined
+    };
+
     p5js_sketch.grid_p5 = ( p ) =>
     {
-        let sketch_data = 
-        {
-            numIter  : 300,
-            targetX  : -0.7436,
-            targetY  : 0.1102,
-            range    : 0.005,
-            gridSize : [ 800, 800 ],
-            image    : undefined
-        };
+        //Shorter name for slightly more readable code
+        let r_data = p5js_sketch.render_data;
 
         let renderMandelbrot = function ()
         {
-            for ( i = 0; i < sketch_data.gridSize[0]; i++ )
+            for ( i = 0; i < r_data.gridSize[0]; i++ )
             {
-                for ( j = 0; j < sketch_data.gridSize[1]; j++ )
+                for ( j = 0; j < r_data.gridSize[1]; j++ )
                 {
                     let n = window.mandelbrot ( 
-                        sketch_data.numIter, 
-                        sketch_data.targetX,
-                        sketch_data.targetY,
-                        sketch_data.range,
-                        sketch_data.gridSize[0],
+                        r_data.numIter, 
+                        r_data.targetX,
+                        r_data.targetY,
+                        r_data.range,
+                        r_data.gridSize[0],
                         i, j                  );
 
                     if ( n == 0 )
                     {
-                        sketch_data.image.set ( i, j, p.color ( 0 ) );
+                        r_data.image.set ( i, j, p.color ( 0 ) );
                     }
                     else
                     {
-                        sketch_data.image.set ( i, j, p.color ( 255 ) );
+                        r_data.image.set ( i, j, p.color ( 255 ) );
                     }
 
                 }
             }
 
-            sketch_data.image.updatePixels ();
+            r_data.image.updatePixels ();
         };
 
         p.setup = function ()
         {
-            let canvas = p.createCanvas ( sketch_data.gridSize [ 0 ],
-                                          sketch_data.gridSize [ 1 ] );
+            let canvas = p.createCanvas ( r_data.gridSize [ 0 ],
+                                          r_data.gridSize [ 1 ] );
 
             canvas.parent ( 'canvasDiv' );
 
-            sketch_data.image = p.createImage ( sketch_data.gridSize [ 0 ],
-                                                sketch_data.gridSize [ 1 ] );
+            r_data.image = p.createImage ( r_data.gridSize [ 0 ],
+                                           r_data.gridSize [ 1 ] );
             renderMandelbrot ();
         }
 
         p.draw = function ()
         {
-            p.image ( sketch_data.image, 0, 0 );
+            p.image ( r_data.image, 0, 0 );
         }
 
         
