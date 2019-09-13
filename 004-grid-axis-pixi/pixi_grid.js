0c7eef3ac82d5602f5886b8be969ac9251311db7
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-09-13 19:44:03 +0100

    Draw a rectangle...

--- 004-grid-axis-pixi/pixi_grid.js
+++ 004-grid-axis-pixi/pixi_grid.js
@@ -1,3 +1,22 @@
 ( function ( pixi_grid, undefined )
 {
+    pixi_grid.init = function ( pixi, app )
+    {
+        let grid_graphics = new pixi.Graphics ();
+
+        app.stage.addChild ( grid_graphics );
+
+        app.ticker.add ( () => grid_draw ( grid_graphics ) );
+    };
+
+    let grid_draw = function ( graphics )
+    {
+        graphics.clear ();
+
+        graphics.beginFill ( 0xFF3300 );
+        graphics.lineStyle ( 10, 0xffd900, 1 );
+
+        graphics.drawRect ( 50, 200, 100, 100 );
+    };
+
 } ( window.pixi_grid = window.pixi_grid || {} ))
